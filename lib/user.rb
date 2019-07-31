259b8653820ec34a18dda64e7d3cdbf33f125f17
Alex Velasquez <jtinnesco@gmail.com>
2019-07-31 01:56:47 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -3,4 +3,9 @@ class User
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
+  end
+  
+  def first_name=(first_name)
+    @first_name = first_name
+  end
 end
